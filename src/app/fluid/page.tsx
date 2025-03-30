b0e18f7dacf6341652da53aa6eeabcce6a15c100
flavioespinoza <flavio.espinoza@gmail.com>
2025-03-30 03:57:15 -0600

    feat: add fluid-tailwind setup and fluid button component

--- src/app/fluid/page.tsx
+++ src/app/fluid/page.tsx
@@ -8,6 +8,8 @@ export default function FluidPage() {
 		<div className="flex min-h-screen flex-col items-center justify-center p-6">
 			<div className="max-w-2xl space-y-8 text-center">
 				<h2 className="text-xl font-semibold">These are just experiments using fluid CSS</h2>
+        <p>Resize the browser window and see the fluid transitions of the elements below.</p>
+        <h1 className="~md/lg:~text-base/4xl">Quick increase</h1>
 				<ButtonFluid onClick={() => alert('Clicked!')}>Fluid Button</ButtonFluid>
 			</div>
 		</div>
