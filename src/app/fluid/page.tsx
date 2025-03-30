3cfd7a0ea473e27c8600f8f51d40add45156b7f4
flavioespinoza <flavio.espinoza@gmail.com>
2025-03-30 04:41:31 -0600

    update: Fluid page.tsx

--- src/app/fluid/page.tsx
+++ src/app/fluid/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import * as React from 'react'
-import ButtonFluid from '@/components/ui/button-fluid'
+import { ButtonFluid } from '@flavioespinoza/salsa-ui'
 
 export default function FluidPage() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center p-6">
 			<div className="max-w-2xl space-y-8 text-center">
 				<h2 className="text-xl font-semibold">These are just experiments using fluid CSS</h2>
-        <p>Resize the browser window and see the fluid transitions of the elements below.</p>
-        <h1 className="~md/lg:~text-base/4xl">Quick increase</h1>
+				<p>Resize the browser window and see the fluid transitions of the elements below.</p>
+				<h1 className="~md/lg:~text-base/4xl">Quick increase</h1>
 				<ButtonFluid onClick={() => alert('Clicked!')}>Fluid Button</ButtonFluid>
 			</div>
 		</div>
