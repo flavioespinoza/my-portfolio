b284219fe8e7edf59605618034aefe1ca9402feb
flavioespinoza <flavio.espinoza@gmail.com>
2025-10-19 10:25:21 -0600

    cleared src

--- src/components/layout/container.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '@/lib/utils'
-
-export function Container({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-	return <div className={cn('mx-auto max-w-5xl px-6', className)} {...props} />
-}
--- src/components/layout/section.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '@/lib/utils'
-
-export function Section({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-	return <section className={cn('space-y-6 py-12', className)} {...props} />
-}
